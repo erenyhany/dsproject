b05c2be4bdb5b1f8329be365c9198c1e8c20d3e1
martin ashraf ibrahim <martin619100@gmail.com>
2023-12-04 12:53:38 +0200

    distribute part 1 tasks

---
+++ src/main/java/ds/datastructprjc/ErrorsHandling.java
@@ -0,0 +1,4 @@
+package ds.datastructprjc;
+
+public class ErrorsHandling {
+}
---
+++ src/main/java/ds/datastructprjc/Functions.java
@@ -0,0 +1,33 @@
+package ds.datastructprjc;
+
+
+//this is an interface for the functions
+public class Functions {
+    public static String detectError(String fileText){
+
+    }
+
+    public static String correctError(String fileText){
+
+    }
+
+    public static String prettify(String fileText){
+
+    }
+
+    public static String minify(String fileText){
+
+    }
+
+    public static String compress(String fileText){
+
+    }
+
+    public static String decompress(String fileText){
+
+    }
+
+    public static String toJSON(String fileText){
+
+    }
+}
