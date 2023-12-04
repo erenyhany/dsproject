8d75a7166f92caea6f28587fa8022c2137019a8c
Mina Hany <105508892+minahany14@users.noreply.github.com>
2023-12-04 14:06:06 +0200

    Update CompDecomp.java

--- src/main/java/ds/datastructprjc/CompDecomp.java
+++ src/main/java/ds/datastructprjc/CompDecomp.java
@@ -1,4 +1,10 @@
 package ds.datastructprjc;
 
 public class CompDecomp {
+  
+  //function removes all spaces and newlines
+  public static String minifying(String input) {
+        return input.replaceAll("\\s", "");
+    }
+  
 }
