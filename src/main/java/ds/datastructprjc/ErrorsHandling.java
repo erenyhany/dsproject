fa0ec4139b35caa6fef561779dc00d904d05d440
martinashraf01 <90205645+martinashraf01@users.noreply.github.com>
2023-12-06 05:00:34 +0200

    Update ErrorsHandling.java start correcting while detecting
    don't forget to make errormsg as a class attribute

--- src/main/java/ds/datastructprjc/ErrorsHandling.java
+++ src/main/java/ds/datastructprjc/ErrorsHandling.java
@@ -1,90 +1,117 @@
 package ds.datastructprjc;
 /*************************/
 
-
 /************************************/
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Stack;
+import java.util.*;
 
 public class ErrorsHandling {
-    //makin a tree to determine the childs of each tags setting children for each node
-    private final static TreeNode id  = new TreeNode("id",null);
-    private final static TreeNode name = new TreeNode("name",null);
-    private final static TreeNode topic = new TreeNode("topic",null);
-    private final static TreeNode body = new TreeNode("body",null);
-    private final static TreeNode topics = new TreeNode("topics",new TreeNode[]{topic,null,null,null});
-    private final static TreeNode followers = new TreeNode("followers",new TreeNode[]{id,null,null,null});
-    private final static TreeNode post = new TreeNode("post",new TreeNode[]{body,topics,null,null});
-    private final static TreeNode posts = new TreeNode("posts",new TreeNode[]{post,null,null,null});
-    private final static TreeNode user = new TreeNode("user",new TreeNode[]{id,name,posts,followers});
-    private final static TreeNode users = new TreeNode("users", new TreeNode[]{user,null,null,null});     //root of the tree
+    private static StringBuilder correctedFile;
+    private static String [] tagNames = {"id","posts","post","users","user","followers","follower","topics","topic","name","body"};
+    //making a tree to determine the childs of each tags setting children for each node
+    private final static TreeNode id  = new TreeNode(tagNames[0],new TreeNode[]{null,null,null,null});
+    private final static TreeNode name = new TreeNode(tagNames[9],new TreeNode[]{null,null,null,null});
+    private final static TreeNode topic = new TreeNode(tagNames[8],new TreeNode[]{null,null,null,null});
+    private final static TreeNode body = new TreeNode(tagNames[10],new TreeNode[]{null,null,null,null});
+    private final static TreeNode topics = new TreeNode(tagNames[7],new TreeNode[]{topic,null,null,null});
+    private final static TreeNode follower = new TreeNode(tagNames[6],new TreeNode[]{id,null,null,null});
+    private final static TreeNode followers = new TreeNode(tagNames[5],new TreeNode[]{follower,null,null,null});
+
+    private final static TreeNode post = new TreeNode(tagNames[2],new TreeNode[]{body,topics,null,null});
+    private final static TreeNode posts = new TreeNode(tagNames[1],new TreeNode[]{post,null,null,null});
+    private final static TreeNode user = new TreeNode(tagNames[4],new TreeNode[]{id,name,posts,followers});
+    private final static TreeNode users = new TreeNode(tagNames[3], new TreeNode[]{user,null,null,null});     //root of the tree
 
 
-    private static Map<String , TreeNode> map ;
+    private static Map<String , TreeNode> map = null;
+
+
+
 
 
     public static String detectErrors(String fileText){
 
         initializeMap();
+
         Stack<String> tagsStack = new Stack<String>();
         int numOflineTags = 0;     //this variable for detecting if  we've missed  '>' and
                                   // in one line we may have multiple tags like opening and closing so
                                     //to differentiate between them , and
+        TreeNode tagBeforePeek = null;
 
 
         boolean boolforFirstElement = true;  //to check the first element in the stack is <users>
         StringBuilder errormsg =new StringBuilder();
+        correctedFile = new StringBuilder(fileText);
         int lineCounter=1;
 
-
-        for( int i = 0 ;i<fileText.length();i++){
+        int iCorrect=0;
+        for( int i = 0 ;i<fileText.length();i++){        //main loop
 
             if(fileText.charAt(i)=='\n'){
                 lineCounter++;
                 numOflineTags=0;
+
             }
 
             if(fileText.charAt(i) =='<'){
                 numOflineTags++;     //increment this variable to know which '<' is not closed by'>' in the same line
                 //the string below (temp) contains what is between < and >
                 String temp = findWordAtIndex(fileText,i+1).replaceAll("\\s", "").toLowerCase();
-                i= i + temp.length();
 
                 //detect if Users tag (special case) doesnt exist
                 if(boolforFirstElement){
                     if(!temp.contains("users") ){
                         errormsg.append("line "+(lineCounter-1)+" :you should add <users> at line \n");
-                        tagsStack.push("users");
+                        correctedFile.delete(0, fileText.indexOf('<'));
+                        correctedFile.insert(0,"<users>\n");
+                        iCorrect =8;
+
                     }
                     boolforFirstElement=false;
                 }
 
-                //detect if '>'is missing in 2 conditions:(1)if temp contains'\n' or (2)if it contains '<'
+                //detect if '>'is missing in one conditions:(1)if temp contains '<'
                 if(temp.contains("<")){
                     errormsg.append("line "+lineCounter+" :the \'<\' no "+numOflineTags+" needs a \'>\'\n");
-                    i = i-temp.length()+temp.indexOf('<');
 
-                    for (Map.Entry<String, TreeNode> entry : map.entrySet()){
-                        if(temp.contains(entry.getKey())){
-                            tagsStack.push(entry.getKey());
+
+                    for(int x=0;x<tagNames.length;x++){
+                        if(temp.substring(0,temp.indexOf('<')).contains(tagNames[x])){
+                            tagBeforePeek = map.get(tagsStack.peek());
+                            tagsStack.push(tagNames[x]);
+                            if(temp.substring(0,temp.indexOf('<')).contains("/")){
+                                correctedFile.insert(iCorrect+2+tagNames[x].length(),">");
+                            }else{
+                                correctedFile.insert(iCorrect+1+tagNames[x].length(),">");
+                            }
+                            iCorrect++;
+                            break;
                         }
                     }
 
+
                 }
 
 
-                //detect if </> is missing
-              /*continue from here */
+                //detect if the tag doesn't have a '/' :
+                // 1) if the top of the stack can be a parent of the current tag the push the current tag
+                // 2)else we should detect the error
                 if(!temp.contains("/")&& !temp.contains("<")){
-                    tagsStack.push(temp);
+                    if(parentAndChild(tagsStack.peek(),temp)){
+                        tagBeforePeek = map.get(tagsStack.peek());
+                        tagsStack.push(temp);
+                    }
                 }
 
             }
+            iCorrect++;
         }
         return errormsg.toString();
     }
+
+
+
 
 
     private static String findWordAtIndex(String text, int startIndex) {
@@ -110,6 +137,7 @@ public class ErrorsHandling {
             map.put("id",id);
             map.put("name",name);
             map.put("followers",followers);
+            map.put("follower",follower);
             map.put("post",post);
             map.put("body",body);
             map.put("topics",topics);
@@ -119,7 +147,15 @@ public class ErrorsHandling {
     }
 
 
+    private static boolean parentAndChild(String parent ,String child){
+        return (map.get(parent)).isParent(map.get(child));
+    }
+
 }
+
+
+
+
 
 
 class TreeNode {
