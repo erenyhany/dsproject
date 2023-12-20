f7af65d5fbba67a1ed8444283c37855dc83d1978
Marten <131295106+Marten235@users.noreply.github.com>
2023-12-20 19:58:48 +0200

    Update JSONHandling.java

--- src/main/java/ds/datastructprjc/JSONHandling.java
+++ src/main/java/ds/datastructprjc/JSONHandling.java
@@ -3,304 +3,302 @@ import java.util.Stack;
 
 
 public class JSONHandling {
-    static public String XMLtoJSON (String xmlString)
+    static String XmltoJson(String xmlString)
     {
-        StringBuilder json = new StringBuilder();
-        boolean makeacoma= false;
+        StringBuffer Json = new StringBuffer();
         Stack<String> stack = new Stack<>();
-
-        boolean infollowers= false;
-
-        String body;
-
-        boolean openTag = false, between = false;
-        StringBuilder content = new StringBuilder();
-        StringBuilder insideTagContent = new StringBuilder();
-
+        boolean open = false;
+        boolean iamthelastone=false;
+        boolean readytogivetag = false;
+        int isarray = 0;
+        StringBuffer insideTagContent = new StringBuffer();
+        Json.append("{"+'\n');
         for (int i = 0; i < xmlString.length(); i++) {
+            insideTagContent.setLength(0);
+            char currentChar = xmlString.charAt(i);
+            readytogivetag = false;
+
+
+            ////////////////////////////////////////////////////////////////////
+            if (currentChar == '<') {
+
+                i++;
+                open = true;
+
+                if (xmlString.charAt(i) == '/') {
+
+
+
+                    open = false;
+                    while (true) {
+                        if (xmlString.charAt(i) == '>') {
+                            break;
+                        }
+                        insideTagContent.append(xmlString.charAt(i));
+                        i++;
+                    }
+                    iamthelastone =givethenexttag(xmlString,i);
+
+                    String st= stack.pop();
+                    if(st==":")
+                    {
+
+                        if(stack.size()>=1&&iamthelastone) {
+                            Json.append(","+'\n');
+                        }
+                        else Json.append('\n');
+
+                    }
+                    else if(st=="[")
+                    {
+                        Json.append("\t".repeat(stack.size()+1)+']');
+                        if(stack.size()>=1&&iamthelastone) {
+                            Json.append(","+'\n');
+                        }
+                        else Json.append('\n');
+                    }
+                    else if(st=="{")
+                    {
+                        Json.append("\t".repeat(stack.size()+1)+'}');
+                        if(stack.size()>=1&&iamthelastone) {
+                            Json.append(","+'\n');
+                        }
+                        else Json.append('\n');
+                    }
+
+
+                }
+                insideTagContent.setLength(0);
+            }
+            while (open) {
+                if (xmlString.charAt(i) == '>') {
+                    open = false;
+
+                    readytogivetag = true;
+                    break;
+                }
+                insideTagContent.append(xmlString.charAt(i));
+                i++;
+            }
+            ////////////////////////////////////////////////////////////////////
+            if (readytogivetag) {
+                isarray = isarray(xmlString,i, insideTagContent.toString());
+
+                if(isarray==1)
+                {
+                    stack.push("[");
+                }
+                else if(isarray==0)
+                {
+                    stack.push("{");
+
+                }
+                else if(isarray==-1)
+                {
+
+                    stack.push(":");
+
+                }
+                Json.append("\t".repeat(stack.size()) +'"'+insideTagContent+'"' +": ");
+                if(isarray==1)
+                {
+                    Json.append("[\n");
+                }
+                else if(isarray==0)
+                {
+
+                    Json.append("{\n");
+                }
+                else if(isarray==-1)
+                {
+                    Json.append( '"'+connnnn(xmlString,i)+'"');
+                }
+
+
+
+            }
+
+
+        }
+        Json.append("}"+'\n');
+
+
+        return (Json.toString());
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+    public static int searchclose(String tag, String xmlString, int i) {
+
+        String closetag = "</" + tag + ">";
+        boolean open = false;
+        StringBuffer insideTagContent = new StringBuffer();
+        for (; i < xmlString.length(); i++) {
             char currentChar = xmlString.charAt(i);
 
-
-
-
-
-
-            if ((insideTagContent.toString()).contains("<users>")) {
-                insideTagContent.setLength(0);
-                stack.push("<users>");
-                json.append("{ \n" + "\t".repeat(stack.size()) + '"' + "users" + '"' + ": ["+"\n");
-                makeacoma=false;
-
-            }
-
-            if ((insideTagContent.toString()).contains("<user>")) {
-                insideTagContent.setLength(0);
-                stack.push("<user>");
-                for (int s = 0; s < stack.size(); s++) {
-                    json.append('\t');
-                }
-                json.append('"' + "user" + '"' + ": {"+"\n");
-
-            }
-
-
-            if ((insideTagContent.toString()).contains("<id>")) {
-                if(infollowers)
-                {
-                    stack.push("<id>");
-                    json.append("\t".repeat(stack.size()+1)+'"' + "id" + '"' + ": ");
-                    content = extractcontent("<id>", i, xmlString, insideTagContent);
-                    json.append(content+"\n");
-                    insideTagContent.setLength(0);
-                    continue;
-                }
-
-                stack.push("<id>");
-                json.append("\t".repeat(stack.size())+'"' + "id" + '"' + ": ");
-                content = extractcontent("<id>", i, xmlString, insideTagContent);
-                json.append(content + ","+"\n");
-                insideTagContent.setLength(0);
-            }
-            if ((insideTagContent.toString()).contains("</id>")) {
-
-                if(infollowers)
-                {
-                    stack.pop();
-
-                    insideTagContent.setLength(0);
-
-
-                    continue;
-                }
-
-                stack.pop();
-
+            if (currentChar == '<' && xmlString.charAt(i+1)=='/')  {
+                open = true;
                 insideTagContent.setLength(0);
             }
 
-            if ((insideTagContent.toString()).contains("<name>")) {
+            while (open) {
+                insideTagContent.append(xmlString.charAt(i));
 
-                stack.push("<name>");
-                for (int s = 0; s < stack.size(); s++) {
-                    json.append('\t');
-
-                }
-                json.append('"' + "name" + '"' + ": ");
-                content = extractcontent("<name>", i, xmlString, insideTagContent);
-                json.append('"' +""+ content +'"' + ","+"\n");
-                stack.pop();
-                insideTagContent.setLength(0);
-
-            }
-
-
-            if ((insideTagContent.toString()).contains("<posts>")) {
-                stack.push("<posts>");
-                for (int s = 0; s < stack.size(); s++) {
-                    json.append('\t');
-                }
-                json.append('"' + "posts" + '"' + ": [");
-                insideTagContent.setLength(0);
-            }
-
-            if ((insideTagContent.toString()).contains("<post>")) {
-                stack.push("<post>");
-                if(makeacoma)
-                {
-                    json.append(",");
-                    makeacoma=false;
-                }
-                json.append("\n"+"\t".repeat(stack.size())+'"' + "post" + '"' + ": {"+"\n");
-                insideTagContent.setLength(0);
-            }
-
-
-            if ((insideTagContent.toString()).contains("<body>")) {
-                stack.push("<body>");
-                for (int s = 0; s < stack.size(); s++) {
-                    json.append('\t');
-                }
-                json.append('"' + "body" + '"' + ": {");
-                content = extractcontent("<body>", i, xmlString, insideTagContent);
-                body=content.toString();
-                body = replaceSubstring(body, "\n", "\n"+"\t".repeat(stack.size()+2)).trim();
-                json.append('"' + body + '"');
-                insideTagContent.setLength(0);
-
-            }
-
-            if ((insideTagContent.toString()).contains("</body>")) {
-
-
-                json.append("},"+"\n");
-                stack.pop();
-                insideTagContent.setLength(0);
-            }
-
-
-            if ((insideTagContent.toString()).contains("<topics>")) {
-                stack.push("<topics>");
-                json.append("\t".repeat(stack.size())+'"' + "topics" + '"' + ": [");
-                insideTagContent.setLength(0);
-
-            }
-
-
-
-            if ((insideTagContent.toString()).contains("<topic>")) {
-
-                if(makeacoma)
-                {
-                    json.append(",");
-                    makeacoma=false;
-                }
-                stack.push("<topic>");
-                content = extractcontent("<topic>", i, xmlString, insideTagContent);
-                body=content.toString().trim();
-                json.append("{");
-                json.append('"' + body + '"');
-                insideTagContent.setLength(0);
-            }
-
-            if ((insideTagContent.toString()).contains("</topic>")) {
-
-
-                stack.pop();
-                json.append("}");
-                insideTagContent.setLength(0);
-                makeacoma=true;
-            }
-
-            if ((insideTagContent.toString()).contains("</topics>")) {
-
-                json.append("]"+"\n");
-                insideTagContent.setLength(0);
-                stack.pop();
-                makeacoma=false;
-
-            }
-
-
-            if ((insideTagContent.toString()).contains("</post>")) {
-
-                makeacoma=true;
-                json.append("\t".repeat(stack.size()+1)+"}");
-                stack.pop();
-                insideTagContent.setLength(0);
-            }
-
-
-
-
-            if ((insideTagContent.toString()).contains("</posts>")) {
-
-                json.append("\n"+"\t".repeat(stack.size()+1)+"],"+"\n");
-                insideTagContent.setLength(0);
-                stack.pop();
-                makeacoma=false;
-
-            }
-
-
-            if ((insideTagContent.toString()).contains("<followers>")) {
-                infollowers=true;
-                stack.push("<followers>");
-                json.append("\t".repeat(stack.size())+'"' + "followers" + '"' + ": ["+"\n");
-                insideTagContent.setLength(0);
-            }
-            if ((insideTagContent.toString()).contains("<follower>")) {
-                stack.push("<follower>");
-                if(makeacoma)
-                {
-                    json.append(","+"\n");
-                    makeacoma=false;
-                }
-                json.append("\t".repeat(stack.size()+1)+"{"+"\n");
-                insideTagContent.setLength(0);
-            }
-
-            if ((insideTagContent.toString()).contains("</follower>")) {
-
-                makeacoma=true;
-                json.append("\t".repeat(stack.size()+1)+"}");
-
-                stack.pop();
-                insideTagContent.setLength(0);
-            }
-
-            if ((insideTagContent.toString()).contains("</followers>")) {
-                infollowers=false;
-                json.append("\n"+"\t".repeat(stack.size()+1)+"]"+"\n");
-                stack.pop();
-                insideTagContent.setLength(0);
-                makeacoma=false;
-
-
-            }
-            if ((insideTagContent.toString()).contains("</user>")) {
-
-                json.append("\t".repeat(stack.size()+1)+"}"+"\n");
-                insideTagContent.setLength(0);
-                stack.pop();
-
-            }
-
-            if (currentChar == '<') {
-                openTag = true;
-                insideTagContent.setLength(0);
-            }
-
-            while (openTag) {
-                insideTagContent.append(currentChar);
-
-                if (currentChar == '>') {
-                    openTag = false;
-                    //System.out.println(insideTagContent);
-
-
+                if (xmlString.charAt(i) == '>') {
+                    open = false;
+                    if (insideTagContent.toString().contains(closetag)) {
+                        return i;
+                    }
                     break;
                 }
 
                 i++;
-                currentChar = xmlString.charAt(i);
 
             }
         }
-
-        json.append("\t\t"+"]"+"\n");
-        json.append("}");
-        return json.toString();
-
+        return 20;
     }
 
 
+    public static String connnnn(String xmlString, int i) {
 
 
-    static StringBuilder extractcontent(String tag, int i, String xmlString, StringBuilder insideTagContent) {
-        StringBuilder content = new StringBuilder();
-        char currentChar = xmlString.charAt(i);
-        boolean between = false;
-        if ((insideTagContent.toString()).contains(tag)) {
-            between = true;
-            content.setLength(0);
-        }
-        while (between) {
-            content.append(currentChar);
-            i++;
-            currentChar = xmlString.charAt(i);
+        boolean open = false;
+        String content = "";
+        i++;
+        while (true)
+        {
+
+            char currentChar = xmlString.charAt(i);
             if (currentChar == '<') {
-                between = false;
-                return content;
+                return (content).trim();
+
             }
+            content = content + currentChar;
+            i++;
+
         }
-        return content;
+
     }
 
-    public static String replaceSubstring(String input, String substringToReplace, String replacementSubstring) {
-        return input.replace(substringToReplace, replacementSubstring);
+
+    public static int isarray(String xmlString, int i, String tag) {
+
+
+        String closetag =  tag ;
+        boolean compare = false;
+        boolean open = false;
+        boolean itisthefirst = true;
+        char currentChar;
+        String insideTagContent = "";
+        String savetag = "";
+        int counter=0;
+
+
+        while (true) {
+            boolean readytogivetag = false;
+
+            ////////////////////////////////////////////////////////////////
+            char cur= xmlString.charAt(i);
+
+            if (xmlString.charAt(i) == '<') {
+                i++;
+                open = true;
+                counter++;
+                insideTagContent = "";
+            }
+            while (open) {
+
+                if (xmlString.charAt(i) == '>') {
+                    open = false;
+                    readytogivetag = true;
+
+                    break;
+                }
+                insideTagContent = insideTagContent + xmlString.charAt(i);
+
+                i++;
+            }
+            ////////////////////////////////////////////////////////////////
+
+            if(readytogivetag) {
+                //see if it was id or name : content
+                if (itisthefirst) {
+                    if (insideTagContent.contains(closetag)) {
+                        return -1;
+                    }
+                }
+                itisthefirst = false;
+                if (insideTagContent.contains(closetag)) {
+
+                    if(counter == 2)
+                    {
+                        //Json.append(d"MArteeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee");
+                        return 0;
+                    }
+
+                    return 1;
+                }
+
+                if (!insideTagContent.contains(closetag)) {
+
+                    if (compare) {
+                        if (!savetag.contains(insideTagContent)) {
+                            return 0;
+                        }
+
+
+                    }
+
+                    savetag = insideTagContent;
+                    compare = true;
+                    i = searchclose(insideTagContent, xmlString, i);
+
+                }
+
+
+            }
+            i++;
+
+
+        }
+
+
+
     }
+
+
+    static boolean givethenexttag(String xml, int i)
+    {
+        while (true&&i<xml.length())
+        {
+
+            if(xml.charAt(i)=='<'&&xml.charAt(i+1)=='/')
+            {
+                return false;
+            }
+            if(xml.charAt(i)=='<')
+            {
+                return true;
+            }
+            i++;
+
+        }
+        return false;
+
+    }
+
+
 }
-
 
 
 
