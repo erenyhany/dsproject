a9fc30f9273f24617da71d878289fe084e64b946
Mina Hany <105508892+minahany14@users.noreply.github.com>
2023-12-27 15:02:23 +0200

    Update Functions.java - 4 functions level2

--- src/main/java/ds/datastructprjc/Functions.java
+++ src/main/java/ds/datastructprjc/Functions.java
@@ -1,7 +1,7 @@
 package ds.datastructprjc;
 
 
-import java.util.Stack;
+import java.util.*;
 
 //this is an interface for the functions
 public class Functions {
@@ -107,5 +107,84 @@ public class Functions {
             }
         }
         return result.toString();
+    }
+
+    public static String mostInfluencer(){
+        int max=0, index=0,n;
+        for(int i=0; i<Graph.adj.size() ; i++){
+            n=(Graph.adj.get(i)).size();
+            if(n>max){
+                max=n;
+                index=i;
+            }
+        }
+        return (XMLReader.UsersList.get(index)).getName();
+    }
+
+    public static String mostActive(){
+        int max=0, index=0,n;
+        for(int i=0; i<XMLReader.UsersList.size() ; i++){
+            //check number of followings if greater than max
+            n=(XMLReader.UsersList.get(i)).followingNumber;
+            if(n>max){
+                max=n;
+                index=i;//set index of user have most followings
+            }
+        }
+        return (XMLReader.UsersList.get(index)).getName();
+    }
+
+    public static String mutualFollowers(User a, User b){
+        List<Integer> nums = new ArrayList<>();
+        int longer = (a.followersIDs.size()>b.followersIDs.size())?1:2; //get which user has more followers
+        int size = (longer==1)?a.followersIDs.size():b.followersIDs.size(); // get max number of followers one has
+        int i=0;
+
+        //search for common followers using User with most followers as reference
+        while(i<size){
+            if(longer ==1){
+                if(b.followersIDs.contains(a.followersIDs.get(i))){
+                    nums.add(a.followersIDs.get(i));
+                }
+                i++;
+            }
+            else if(longer ==2) {
+                if (a.followersIDs.contains(b.followersIDs.get(i))) {
+                    nums.add(b.followersIDs.get(i));
+                }
+                i++;
+            }
+        }
+
+        //add names to string
+        String names="";
+        for(int j=0;j<nums.size();j++){
+            names=names.concat((XMLReader.UsersList.get(nums.get(j)-1)).getName());
+            names=names.concat(", ");
+        }
+
+        return names;
+    }
+
+    public static String suggestFollow(User a){
+        LinkedList<Integer> temp = new LinkedList<>();
+        List<Integer> nums = new ArrayList<>();
+        for(int i=0; i<a.followersIDs.size(); i++){
+            temp = (XMLReader.UsersList.get(a.followersIDs.get(i)-1)).followersIDs;
+
+            for(int j=0; j<temp.size(); j++){
+                if(!(a.followersIDs.contains(temp.get(j))) && !nums.contains(temp.get(j)) && (temp.get(j) != a.getID())){
+                    nums.add(temp.get(j));
+                }
+            }
+
+        }
+        String names="";
+        for(int j=0;j<nums.size();j++){
+            names=names.concat((XMLReader.UsersList.get(nums.get(j)-1)).getName());
+            names=names.concat(", ");
+        }
+
+        return names;
     }
 }
